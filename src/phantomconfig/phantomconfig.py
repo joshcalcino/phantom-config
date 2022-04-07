b22ab18b3c324095d5e0c243295709cb9aeffe8a
joshcalcino <josh.calcino@gmail.com>
2022-04-06 18:37:28 -0600

    Allow lists to change to other value

--- src/phantomconfig/phantomconfig.py
+++ src/phantomconfig/phantomconfig.py
@@ -419,7 +419,8 @@ class PhantomConfig:
         tmp = self.config[variable]
 
         if not isinstance(value, type(tmp[1])):
-            raise ValueError('Value and variable are not compatible')
+            if not isinstance(type(tmp[1]), List):
+                raise ValueError('Value and variable are not compatible')
 
         self.config[variable] = ConfigVariable(tmp[0], value, tmp[2], tmp[3])
 
